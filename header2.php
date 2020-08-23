67d8b81f7058f65cecc00c31013406009513ce27
jmason32 <jordanmason32@gmail.com>
2020-08-22 22:31:18 -0400

    Added bussiness/add.php for users to add businesses, also made a header2.php to be included in files that holds the menu and top header

---
+++ header2.php
@@ -0,0 +1,20 @@
+<div id="menu-list" data-selected="menu-pages" data-load="menu/menu-list.php" data-height="415" class="menu-box menu-load menu-bottom"></div>    
+<div id="menu-demo" data-load="menu/menu-demo.php" data-height="210" class="menu-box menu-load menu-bottom"></div>
+<div id="menu-find" data-load="menu/menu-find.php" data-height="420" class="menu-box menu-load menu-bottom"></div>
+
+<div class="header header-scroll-effect">
+    <div class="header-line-1 header-hidden header-logo-app">
+        <a href="#" class="back-button header-logo-title">Back to Pages</a>
+        <a href="#" class="back-button header-icon header-icon-1"><i class="fa fa-angle-left"></i></a>
+        <a href="#" data-menu="menu-find" class="header-icon header-icon-3"><i class="fa fa-search"></i></a>
+        <a href="#" data-menu="menu-list" class="header-icon header-icon-4"><i class="fa fa-bars"></i></a>
+        <a href="#" data-menu="menu-demo" class="header-icon header-icon-2"><i class="fa fa-cog"></i></a>
+    </div>
+    <div class="header-line-2 header-scroll-effect">
+        <a href="#" class="header-pretitle header-date color-highlight"><!--Date will Appear Here --></a>
+        <a href="#" class="header-title">Profile Page</a>
+        <a href="#" data-menu="menu-list" class="header-icon header-icon-1"><i class="fa fa-bars"></i></a>
+        <a href="#" data-menu="menu-find" class="header-icon header-icon-2"><i class="fa fa-search"></i></a>
+        <a href="#" data-menu="menu-demo" class="header-icon header-icon-3"><i class="fa fa-cog"></i></a>
+    </div>  
+</div>
--- index.php
+++ index.php
@@ -7,32 +7,13 @@
     }
     
 ?>
+
+
 <main>
 
         
     <div id="page-transitions" class="page-build light-skin highlight-blue">    
-        <div id="menu-hider"></div>
-        
-        <div id="menu-list" data-selected="menu-pages" data-load="menu/menu-list.php" data-height="415" class="menu-box menu-load menu-bottom"></div>    
-        <div id="menu-demo" data-load="menu/menu-demo.php" data-height="210" class="menu-box menu-load menu-bottom"></div>
-        <div id="menu-find" data-load="menu/menu-find.php" data-height="420" class="menu-box menu-load menu-bottom"></div>
-
-        <div class="header header-scroll-effect">
-            <div class="header-line-1 header-hidden header-logo-app">
-                <a href="#" class="back-button header-logo-title">Back to Pages</a>
-                <a href="#" class="back-button header-icon header-icon-1"><i class="fa fa-angle-left"></i></a>
-                <a href="#" data-menu="menu-find" class="header-icon header-icon-3"><i class="fa fa-search"></i></a>
-                <a href="#" data-menu="menu-list" class="header-icon header-icon-4"><i class="fa fa-bars"></i></a>
-                <a href="#" data-menu="menu-demo" class="header-icon header-icon-2"><i class="fa fa-cog"></i></a>
-            </div>
-            <div class="header-line-2 header-scroll-effect">
-                <a href="#" class="header-pretitle header-date color-highlight"><!--Date will Appear Here --></a>
-                <a href="#" class="header-title">Profile Page</a>
-                <a href="#" data-menu="menu-list" class="header-icon header-icon-1"><i class="fa fa-bars"></i></a>
-                <a href="#" data-menu="menu-find" class="header-icon header-icon-2"><i class="fa fa-search"></i></a>
-                <a href="#" data-menu="menu-demo" class="header-icon header-icon-3"><i class="fa fa-cog"></i></a>
-            </div>  
-        </div>
+        <?php require 'header2.php';?>
         
         <div class="page-content header-clear-large">
                 
